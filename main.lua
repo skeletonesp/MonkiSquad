70b964b4e504ce5c05003def06231138e6c27627
pxw3r <pxwerzapasowe@int.pl>
2022-10-27 20:40:47 +0200

    Brzesko

---
+++ main.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 10042681323 then -- BrzeskoRP
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/A2.lua", true))()
+    else if game.PlaceId == 10758111998 then -- Booga Booga Reborn
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/Shoot-Destroyer", true))()
+    end   
+end
