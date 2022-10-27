ec6632b3c9571cc8ddfb299c10593378e9176f62
pxw3r <pxwerzapasowe@int.pl>
2022-10-27 21:30:57 +0200

    main

--- main.lua
+++ main.lua
@@ -2,5 +2,7 @@ if game.PlaceId == 10042681323 then -- BrzeskoRP
     loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/brzesko.lua", true))()
     else if game.PlaceId == 10758111998 then -- Booga Booga Reborn
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/Shoot-Destroyer", true))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/Shoot-Destroyer", true))()
     end   
 end
