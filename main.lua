97c43f3435125e1bd970276384589d6ccd1a9b40
pxw3r <94073902+pxw3r@users.noreply.github.com>
2022-10-29 01:20:35 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ if game.PlaceId == 10042681323 then -- brzesko
     loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/brzesko.lua", true))()
     else if game.PlaceId == 33333 then --e
         loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/Shoot-Destroyer", true))()
-        else if game.PlaceId == 45819666153 then -- anomic
+        else if game.PlaceId == 4581966615 then -- anomic
             loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/anomic.lua", true))()
             else
                loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal.lua", true))()           
