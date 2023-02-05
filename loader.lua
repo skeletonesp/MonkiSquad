a000207c08d8de16dde10b3cbc215fa12e02e6bf
pxw3r <94073902+pxw3r@users.noreply.github.com>
2023-02-05 18:04:02 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+		if game.PlaceId == 10042681323 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/brzesko.lua", true))()
+		else if game.PlaceId == 33333 then
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/Shoot-Destroyer", true))()
+			else if game.PlaceId == 4581966615 then
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/anomic.lua", true))()
+				else
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal.lua", true))()           
+				end
+			end
+		end
