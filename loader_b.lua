a70bf758db8af6e8aaf069f38463f919018dd241
pxw3r <94073902+pxw3r@users.noreply.github.com>
2023-02-05 19:09:05 +0100

    Update loader_b.lua

--- loader_b.lua
+++ loader_b.lua
@@ -1,7 +1,7 @@
 		if game.PlaceId == 2413927524 then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/rake.lua", true))()
-		else if game.PlaceId == 0 then
-				loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()
+		else if game.PlaceId == 730951264 then
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/maze.lua", true))()
 			else if game.PlaceId == 1 then
 					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()
 				else
