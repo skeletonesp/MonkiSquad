ea97b1d2ab457379881be83c54c07284972b0f17
pxw3r <94073902+pxw3r@users.noreply.github.com>
2023-02-05 18:06:01 +0100

    Create loader_b.lua

---
+++ loader_b.lua
@@ -0,0 +1,11 @@
+		if game.PlaceId == 2413927524 then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/rake.lua", true))()
+		else if game.PlaceId == 0 then
+				loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()
+			else if game.PlaceId == 1 then
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()
+				else
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()           
+				end
+			end
+		end
