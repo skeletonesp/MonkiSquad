c5710a7f3553febdb6b74ebaf623a8344604df66
pxw3r <94073902+pxw3r@users.noreply.github.com>
2023-02-15 23:16:01 +0100

    Update loader_b.lua

--- loader_b.lua
+++ loader_b.lua
@@ -3,7 +3,7 @@
 		else if game.PlaceId == 730951264 then
 				loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/maze.lua", true))()
 			else if game.PlaceId == 2788229376 or 5602055394 then
-					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()
+					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MonkiSquad/main/dahood.lua", true))()
 				else
 					loadstring(game:HttpGet("https://raw.githubusercontent.com/pxw3r/MS-Loadstring/main/universal25.lua", true))()           
 				end
